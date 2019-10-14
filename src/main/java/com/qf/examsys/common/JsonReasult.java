3e9e8f69615c5f537b47587378e80de310f862e2
sss <980951644@qq.com>
2019-10-14 16:54:14 +0800

    JsonResult修改格式

--- src/main/java/com/qf/examsys/common/JsonReasult.java
+++ src/main/java/com/qf/examsys/common/JsonReasult.java
@@ -3,6 +3,23 @@ package com.qf.examsys.common;
 public class JsonReasult<T> {
     private Integer code;
     private T data;
+    private String msg;
+    private Integer count;
+
+    public JsonReasult() {
+    }
+
+    public JsonReasult(Integer code, T data) {
+        this.code = code;
+        this.data = data;
+    }
+
+    public JsonReasult(Integer code, T data, String msg, Integer count) {
+        this.code = code;
+        this.data = data;
+        this.msg = msg;
+        this.count = count;
+    }
 
     public Integer getCode() {
         return code;
@@ -18,5 +35,31 @@ public class JsonReasult<T> {
 
     public void setData(T data) {
         this.data = data;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public Integer getCount() {
+        return count;
+    }
+
+    public void setCount(Integer count) {
+        this.count = count;
+    }
+
+    @Override
+    public String toString() {
+        return "JsonReasult{" +
+                "code=" + code +
+                ", data=" + data +
+                ", msg='" + msg + '\'' +
+                ", count=" + count +
+                '}';
     }
 }
